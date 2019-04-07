e0c18730ecc6e6b9f7593246a33a01b18cd63672
Josega149 <jg.tamura10@uniandes.edu.co>
2019-04-07 18:21:45 -0500

    working

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,3 +1,5 @@
+//exp build:android
+//exp build:status
 import React, { Component } from 'react';
 import {
   Text,
@@ -29,6 +31,12 @@ export default class AgendaScreen extends Component {
     return (
       <Agenda
       //  displayLoadingIndicator={true}
+        renderKnob={() => {return (<Icon.Ionicons
+          name={'md-create'}
+          size={26}
+          //style={{ marginBottom: -3 }}
+          color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+        />);}}
         items={this.state.monthEvents}
         firstDay={1}
         selected={new Date()}
@@ -50,25 +58,31 @@ export default class AgendaScreen extends Component {
   }
 
   getMonthEventsDaybyDay(day){
+    console.log(day)
+
     const ev1 = new EventClass({ 
       year: 2019, month: 4, day: 10, hour: 10, minute: 0, duration: 90, 
-      title: "1", category: "TRANSPORT", tags: ["SITP"],
-      ammount: 2200, type: EventClass.EXPENSE, account: "BANCOLOMBIA_DEBITO" 
+      title: "1", category: EventClass.categories.TRANSPORT, tags: ["SITP"],
+      ammount: 2200, type: EventClass.types.EXPENSE, 
+      account: EventClass.accounts.DEBITO_BANCOLOMBIA 
     });
     const ev2 = new EventClass({ 
       year: 2019, month: 4, day: 10, hour: 10, minute: 0, duration: 90, 
-      title: "2", category: "TRANSPORT", tags: ["SITP"],
-      ammount: 2200, type: EventClass.INCOME, account: "BANCOLOMBIA_DEBITO" 
+      title: "2", category: EventClass.categories.TRANSPORT, tags: ["SITP"],
+      ammount: 2200, type: EventClass.types.INCOME, 
+      account: EventClass.accounts.DEBITO_BANCOLOMBIA 
     });
     const ev3 = new EventClass({ 
       year: 2019, month: 4, day: 11, hour: 10, minute: 0, duration: 90, 
-      title: "3", category: "TRANSPORT", tags: ["SITP"],
-      ammount: 2200, type: EventClass.EXPENSE, account: "BANCOLOMBIA_DEBITO" 
+      title: "3", category: EventClass.category.TRANSPORT, tags: ["SITP"],
+      ammount: 2200, type: EventClass.types.EXPENSE, 
+      account: EventClass.accounts.DEBITO_BANCOLOMBIA 
     });
     const ev4 = new EventClass({ 
       year: 2019, month: 4, day: 20, hour: 10, minute: 0, duration: 90, 
-      title: "4", category: "TRANSPORT", tags: ["SITP"],
-      ammount: 2200, type: EventClass.NEUTRAL, account: "BANCOLOMBIA_DEBITO" 
+      title: "4", category: EventClass.categories.TRANSPORT, tags: ["SITP"],
+      ammount: 0, type: EventClass.types.NEUTRAL, 
+      account: EventClass.accounts.DEBITO_BANCOLOMBIA 
     });
 
     let eventList = [ev1, ev2, ev3, ev4];
@@ -76,20 +90,29 @@ export default class AgendaScreen extends Component {
     let dayEvents = {};
     let monthEvents = {};
     let markedDates = {}
-    
+
+    const accounts = EventClass.accounts;
+    const keys = Object.keys(accounts);
+    console.log(keys);
+
     for(let i=0; i < eventList.length; i++ ) {
       const event = eventList[i];
       if(!dayEvents[event.getYearMonthDay()]){
-        dayEvents[event.getYearMonthDay()] = { dayExpenses: 0, dayIncome: 0 }
+        dayEvents[event.getYearMonthDay()] = { };
+        for(let j = 0; j< keys.length; j++ ){
+          dayEvents[event.getYearMonthDay()][accounts[keys]].dayIncomes = 0;
+          dayEvents[event.getYearMonthDay()][accounts[keys]].dayExpenses = 0;
+        }
+
         monthEvents[event.getYearMonthDay()] = []
-        markedDates[event.getYearMonthDay()] = {marked: true, dots: []}
+        markedDates[event.getYearMonthDay()] = {startingDay: true, selected: true, endingDay: true, color: 'green', textColor: 'gray', marked: true, dots: []}
       }
 
       if(event.type === EventClass.EXPENSE) {
-        dayEvents[event.getYearMonthDay()].dayExpenses += event.ammount;
+        dayEvents[event.getYearMonthDay()][event.account].dayExpenses += event.ammount;
         markedDates[event.getYearMonthDay()]['dots'].push(this.state.expense);
       } else if (event.type === EventClass.INCOME) {
-        dayEvents[event.getYearMonthDay()].dayIncome += event.ammount;
+        dayEvents[event.getYearMonthDay()][event.account].dayIncome += event.ammount;
         markedDates[event.getYearMonthDay()]['dots'].push(this.state.income);
       } else if (event.type === EventClass.NEUTRAL) {
         markedDates[event.getYearMonthDay()]['dots'].push(this.state.neutral);
@@ -98,21 +121,23 @@ export default class AgendaScreen extends Component {
       monthEvents[event.getYearMonthDay()].push(event.getEvent());
     }
     //fill dates with no events
+    
     for (let i = 1; i < 32; i++) {
       const date = `${day.year}-${day.month < 10 ? '0' + day.month : day.month}-${i < 10 ? '0' + i : i}`;
-      if (!monthEvents[date]) {
-        monthEvents[date] = []
-      }
+      const dayEvent = dayEvents[date];
+      let evReport = new EventClass({
+        year: 2019, month: 1, day: 1, hour: 1, minute: 0, duration: 90,
+        title: "Report at end day", category: EventClass.categories.REPORT, tags: [],
+        ammount: 0, type: EventClass.types.NEUTRAL,
+        account: EventClass.accounts.NONE,
+        dayEvent: dayEvent
+      });
+      monthEvents[date].unshift(evReport.getEvent());
     }
-    /*let monthEvents = {
-      "2019-03-10": [
-        ev1.getEvent(), 
-        
-      ],
-      "2019-03-11": [],
-      "2019-03-15": []
-    };*/
-    console.log(monthEvents)
+    /*
+      Supuestamente a este punto ya crea un evento al inicio del día informando
+      los gastos del día
+    */
 
     this.setState({
       dayEvents: dayEvents,
@@ -124,8 +149,6 @@ export default class AgendaScreen extends Component {
   //calendar_view
   // Send to agenda all the events for the month
   loadEventsForMonth(day) {
-    console.log("is loading events for month");
-    console.log(day)
     setTimeout(() => {
       this.getMonthEventsDaybyDay(day);
     }, 1000);
--- screens/Event_view.js
+++ screens/Event_view.js
@@ -6,14 +6,29 @@ import {
   TextInput,
   Text
 } from 'react-native';
+import EventClass from './eventClass';
 
 export default class Event_view extends Component {
 
   render() {
+    let style = this.props.style;
+    let event = this.props.event;
+    if( event.category === EventClass.categories.REPORT) {
+      style.backgroundColor = 'blue'
+    }
       return(
-        <View style={this.props.style}>
+        <View style={style}>
           <Text>
             {this.props.event.title}
+            
+            {/**Ponerle la hora y color dependiendo del tipo de evento */}
+
+            {this.props.event.dayEvent !== null &&
+              "Today's income: " + this.props.event.dayEvent.dayIncome + "\n" +
+              "Today's expenses: " + this.props.event.dayEvent.dayExpenses + "\n" 
+              //this.props.
+
+            }
           </Text>
         </View>
       )
